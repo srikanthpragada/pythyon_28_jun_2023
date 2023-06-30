58c716817c055a8fc3683d214f04ca683a3f05e2
srikanthpragada <srikanthpragada@gmail.com>
2023-06-30 19:58:27 +0530

    30-JUN

---
+++ demo/even_odd.py
@@ -0,0 +1,7 @@
+# Show whether a number is even or odd
+
+num = int(input("Enter number :"))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
---
+++ demo/net_price.py
@@ -0,0 +1,7 @@
+# Take price and display net price after a discount of 10%
+
+price = float(input("Enter price :"))
+discount = price * .10
+netprice = price - discount
+
+print('Net price =', netprice)
